63d716905b69ee93dac0dcf68dd80a663586ba62
Jaden <jadenglenn@pursuit.org>
2024-05-16 16:07:28 -0400

    Navigation set up

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="border border-black border-opacity-100 mt-8 m-4">
+        <h1 className="text-gray-700 text-center pb-6 ">PlaceHolder for Title</h1>
+      <div>
+        <ul className="flex flex-row place-content-evenly">
+        <Link to="/"><li className="list-none">Landing</li></Link>
+        <Link to="/schools"><li className="list-none">All Schools</li></Link>
+        <Link to="/find"><li className="list-none">Find a School</li></Link>
+        </ul>
+      </div>
+    </div>
+  )
 };
 
 export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/App.jsx"
+    "./src/**/*.{js,jsx,ts,tsx,html}"
   ],
   theme: {
     extend: {},
