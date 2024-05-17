7d9c6d004c91a2e3266c1c9cc72711a50e8d9f07
Carlitos <carlitosdutan@pursuit.org>
2024-05-17 13:21:27 -0400

    added styling to borough filter

--- src/BoroughFilter.jsx
+++ src/BoroughFilter.jsx
@@ -30,47 +30,47 @@ const BoroughFilter = ({
   }
 
   return (
-    <div className="text-center h-fit border-2 border-black mx-4 p-4 rounded-lg">
+    <div className="text-center h-fit border-2 border-black mx-4 p-4 rounded-lg bg-ghost-white">
       <h1 className="font-bold">Filter By Borough:</h1>
       {selectedBorough && (
         <button
           onClick={removeFilteredBorough}
-          className="border-2 border-black m-2 p-1 rounded-lg"
+          className="border-2 border-black m-2 p-1 rounded-lg bg-light-blue"
         >
           {`${selectedBorough}❌`}
         </button>
       )}
       <div className="flex flex-col">
         <button
-          className=" border-2 border-black m-2 p-1 rounded-lg"
+          className=" border-2 border-black m-2 p-1 rounded-lg bg-light-blue"
           value={[24, 25, 26, 27, 28, 29, 30]}
           onClick={filterBasedOnBorough}
         >
           Queens
         </button>
         <button
-          className="border-2 border-black m-2 p-1 rounded-lg"
+          className="border-2 border-black m-2 p-1 rounded-lg bg-light-blue"
           value={[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 32]}
           onClick={filterBasedOnBorough}
         >
           Brooklyn
         </button>
         <button
-          className="border-2 border-black m-2 p-1 rounded-lg"
+          className="border-2 border-black m-2 p-1 rounded-lg bg-light-blue"
           value={[1, 2, 3, 4, 5, 6]}
           onClick={filterBasedOnBorough}
         >
           Manhattan
         </button>
         <button
-          className="border-2 border-black m-2 p-1 rounded-lg"
+          className="border-2 border-black m-2 p-1 rounded-lg bg-light-blue"
           value={[7, 8, 9, 10, 11, 12]}
           onClick={filterBasedOnBorough}
         >
           Bronx
         </button>
         <button
-          className="border-2 border-black m-2 p-1 rounded-lg"
+          className="border-2 border-black m-2 p-1 rounded-lg bg-light-blue"
           value={31}
           onClick={filterBasedOnBorough}
         >
